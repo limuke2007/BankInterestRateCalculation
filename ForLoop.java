eeab139f9a95a95850bd0bb28906d9d105331398
Limu Ke <62216538+limuke2007@users.noreply.github.com>
2020-06-14 22:50:50 -0500

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -3,7 +3,7 @@ package java_masterclass;
 public class ForLoop {
     public static void main(String[] args) {
 
-        for (double i = 2; i < 9; i = i + 0.5 ) {
+        for (double i = 2; i < 9; i += 0.5 ) {
             System.out.println("10,000 at " + i +"% interest = " + String.format("%.2f" ,calculatedInterest(10000.0, i)) );
         }
     }
