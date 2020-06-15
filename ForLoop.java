ee084480bd26620ad478e1af03be04f3093728ad
Limu Ke <62216538+limuke2007@users.noreply.github.com>
2020-06-14 21:58:50 -0500

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -11,3 +11,20 @@ public class ForLoop {
         return (amount*(interestRate/100));
     }
 }
+
+/* printed output: 
+
+10,000 at 2.0% interest = 200.00
+10,000 at 2.5% interest = 250.00
+10,000 at 3.0% interest = 300.00
+10,000 at 3.5% interest = 350.00
+10,000 at 4.0% interest = 400.00
+10,000 at 4.5% interest = 450.00
+10,000 at 5.0% interest = 500.00
+10,000 at 5.5% interest = 550.00
+10,000 at 6.0% interest = 600.00
+10,000 at 6.5% interest = 650.00
+10,000 at 7.0% interest = 700.00
+10,000 at 7.5% interest = 750.00
+10,000 at 8.0% interest = 800.00
+10,000 at 8.5% interest = 850.00
